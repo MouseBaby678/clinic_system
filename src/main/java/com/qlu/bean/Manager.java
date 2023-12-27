e52deaad29907d1b6d0f1343f54d7d47a395448e
kaorouguo <z2726803905@outlook.com>
2023-12-27 17:30:15 +0800

    管理员登录

--- src/main/java/com/qlu/bean/Manager.java
+++ src/main/java/com/qlu/bean/Manager.java
@@ -1,6 +1,15 @@
 package com.qlu.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Manager {
-    String Manager_name;
-    String Manager_password;
+    private Integer manager_id;
+
+    private String manager_name;
+    private String manager_password;
 }
--- src/main/java/com/qlu/mapper/ManagerMapper.java
+++ src/main/java/com/qlu/mapper/ManagerMapper.java
@@ -1,4 +1,8 @@
 package com.qlu.mapper;
 
+import com.qlu.bean.Manager;
+import org.apache.ibatis.annotations.Param;
+
 public interface ManagerMapper {
+    Manager selectByUsernameAndPassword(@Param("manager_name") String manager_name,@Param("manager_password") String manager_password);
 }
