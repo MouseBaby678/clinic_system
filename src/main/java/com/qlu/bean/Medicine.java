70d6434b2106e9c5fdc49e00099f254580d837ee
en2mouse <en2mouse@outlook.com>
2023-12-28 16:45:22 +0800

    更新了药品管理的相关内容，页面有微小改动

--- src/main/java/com/qlu/bean/Medicine.java
+++ src/main/java/com/qlu/bean/Medicine.java
@@ -1,4 +1,14 @@
 package com.qlu.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Medicine {
+    private Integer medicine_id;
+    private String medicine_name;
+    private Double medicine_price;
 }
