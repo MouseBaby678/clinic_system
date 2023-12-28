f1adead973f4fbd3d462eeff209d55636bccdd35
MouseBaby <2115395065@qq.com>
2023-12-28 15:47:13 +0800

    增加@RequestMapping

--- src/main/java/com/qlu/controller/DoctorController.java
+++ src/main/java/com/qlu/controller/DoctorController.java
@@ -6,22 +6,24 @@ import com.qlu.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/manger/doctor")
 public class DoctorController {
 
     @Autowired
     private DoctorService doctorService;
-    @GetMapping("/doctor/{id}")
+    @GetMapping("/{id}")
     public RespBean selectById(@PathVariable("id") Integer id) {
         Doctor doctor = doctorService.selectById(id);
         return RespBean.ok("查询成功", doctor);
     }
 
-    @GetMapping("/doctor")
+    @GetMapping()
     public RespBean selectAll() {
         List<Doctor> doctorList = doctorService.selectAll();
         return RespBean.ok("查询成功", doctorList);
